2d87ee7731df2f84658f113bf62dd33139e147c1
Bruna <bferreira.slv@gmail.com>
2022-04-13 22:44:41 -0300

    atualização comendos

--- botlive/bot.py
+++ botlive/bot.py
@@ -126,11 +126,148 @@ class Bot(commands.Bot):
             '''https://github.com/feministech'''
         )
 
-    # @commands.command(name='evento')
-    # async def cmd_evento(self, ctx):
-    #     await ctx.send(
-    #         f'{ctx.author.name}  '
-    #     )
+    @commands.command(name='evento')
+    async def cmd_evento(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Dia 23, às 12:45, no canal da Morganna
+            estaremos realizando o nosso evento Feministalk: Carreiras
+            para compartilharmos juntes um excelente conteúdo, contando
+            com apoio de pessoas profissionais da área. Aproveitem para
+            compartilhar, fazer networking e tirarem dúvidas. Para uma
+            boa experiência para todes, conheçam nosso Código de Conduta:
+            digite !conduta no chat.'''
+        )
+        await ctx.send(
+            '''!sh @morgannadev
+            '''
+        )
+
+    @commands.command(name='rodadeconversa')
+    async def cmd_rodadeconversa(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Teremos uma Roda de Conversa para falar
+            com pessoas profissionais maravilhosas da área de recrutamento
+            e tecnologia sobre carreira e diversas dicas deste universo.
+            Teremos a presença de !marianna, !bru, !jamille, !jessica, !kamila,
+            !isabela, !leticia. Conheça mais sobre elas digitando o
+            comando no chat.'''
+        )
+
+    @commands.command(name='palestras')
+    async def cmd_palestras(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} 13h00 - 13h30: !silva como keynote do
+            tema "Conhecendo Silvia Coelho: inspire-se em sua história"
+            || 13h35 - 14h05: !paula com o tema "O mundo das mentorias e
+            suas surpresas" '''
+        )
+        await ctx.send(
+            ''' 14h10 - 14h40: !monique com o tema "Papo
+            sobre vida de pessoa desenvolvedora e empreendedora" ||
+            14h45 - 15h15: !cami com o tema -em breve- ||
+            15h20 - 16h00: !isabelle com o tema "PJotinha, vale mesmo a
+            pena?" || 16h05 - 17h30: !rodadeconversa
+            '''
+        )
+
+    @commands.command(name='silvia')
+    async def cmd_silvia(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Silvia Coelho (ela/dela) é fundadora
+            do Elas Programam e estará em nosso keynote para contar sua
+            história. Conheça e acompanhe seu trabalho:
+            https://instagram.com/elasprogramam'''
+        )
+
+    @commands.command(name='paula')
+    async def cmd_paula(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Paula Santana (ela/dela) é especialista
+            de desenvolvimento, educadora, mentora, pagando os boletos com
+            Java desde 2014, apaixonada por viajar, mãe de gato e cansada.
+            É também palestrante do tema “O mundo das mentorias e suas
+            surpresas”. Conheça e acompanhe seu trabalho:
+            https://twitter.com/psanrosa13 e
+            https://www.linkedin.com/in/paula-macedo-santana-dev/'''
+        )
+
+    @commands.command(name='monique')
+    async def cmd_monique(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Monique Oliveira (ela/dela) é empreendedora
+            e nerd raíz. E também palestrante do tema: “Papo sobre vida de
+            pessoa desenvolvedora e empreendedora”. Conheça e acompanhe seu
+            trabalho: https://twitter.com/moniquelive e
+            https://www.instagram.com/moniquelive.dev/'''
+        )
+
+    @commands.command(name='cami')
+    async def cmd_cami(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Cami Martins (ele/ela https://pronoun.is/they)
+            é punk, paulista, santista. Docker Community Leader. Sou Senior Site
+            Reliability Engineer e atualmente trabalho na Storyblok, com pós em
+            Forense Computacional e mestrando em Operação, avaliação e gerenciamento
+            avançado de redes de computadores na Universidade Federal do Estado do
+            Rildy Janeiro hehe.'''
+        )
+        await ctx.send(
+            ''' Nas horas vagas, estou codando (também!) e surtando
+            com uma gatinha preta chamada Marvel (!) <3.
+            Conheça e acompanhe seu trabalho:
+            https://punkdodevops.com
+            '''
+        )
+
+    @commands.command(name='isabelle')
+    async def cmd_isabelle(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Isabelle Samways (ela/dela) é Pedreira
+            de Software, aka dev backend. É também palestrante do tema
+            “PJotinha, vale mesmo a pena?”. Conheça e acompanhe seu
+            trabalho: https://twitter.com/bellesamways'''
+        )
+
+    @commands.command(name='marianna')
+    async def cmd_marianna(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Marianna Costa (ela/dela) é Tech Recruiter.
+            É também participante da nossa roda de conversa sobre Carreiras
+            em Tecnologia. Conheça e acompanhe seu trabalho:
+            https://twitter.com/ITrecruitermari e
+            https://www.linkedin.com/in/mariannapcosta/'''
+        )
+
+    @commands.command(name='bru')
+    async def cmd_bru(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Bru (ela/dela) é Vendedora
+            de Oportunidades. É também participante da nossa
+            roda de conversa sobre Carreiras em Tecnologia.
+            Conheça e acompanhe seu trabalho:
+            https://www.linkedin.com/in/bruna-s-b2700b46/'''
+        )
+
+    @commands.command(name='jamille')
+    async def cmd_jamille(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Jamille de Oliveira
+            (ela/dela) é Analista de Atração e Seleção
+            de Talentos. É também participante da nossa
+            roda de conversa sobre Carreiras em Tecnologia.
+            Conheça e acompanhe seu trabalho:
+            https://www.linkedin.com/in/jamille-de-oliveira-26b4756b/'''
+        )
+
+    @commands.command(name='kamila')
+    async def cmd_kamila(self, ctx):
+        await ctx.send(
+            f'''{ctx.author.name} Kamila Santos (ela/dela)
+            é Desenvolvedora backend e criadora de conteúdo
+            kamila code. É também participante da nossa roda
+            de conversa sobre Carreiras em Tecnologia. Conheça
+            e acompanhe seu trabalho: https://beacons.page/kamila_code'''
+        )
 
     @commands.command(name='rainbow')
     async def cmd_rbw(self, ctx):
